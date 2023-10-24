07d1266c043b12920ae7b24d7dfb2b9f0bbba10a
MI KE <fernandesmandrew@gmail.com>
2023-10-24 17:50:12 +0800

    Added all the necessary elements and components

--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -4,6 +4,7 @@ import style from "./Signin.module.css";
 
 // Assets
 import Brand from "../../assets/branding/brand-dark.svg";
+import FbIcon from "../../assets/icons/fb-square.svg";
 
 const Signin = () => {
   return (
@@ -16,16 +17,21 @@ const Signin = () => {
         />
       </div>
       <div className={style["vp-content"]}>
-        <h1 className="prm-text">Welcome to Notefyre</h1>
+        <h1 className="prm-text">All your notes in one virtual place</h1>
         <p className="paragraph">
-          Jot down your notes anytime and anywhere. Just a single click!
+          A simple and minimalistic note taking companion to help you keep and
+          preserve your everyday learnings.
         </p>
       </div>
       <div className={style["cta-container"]}>
         <a className={style["fb-prompt"]} href="">
+          <img src={FbIcon} alt="Facebook Icon" />
           Sign in using Facebook
         </a>
         <a href="">Nah, skip for now</a>
+        <a className={style["cta-register"]} href="">
+          Don't have an account? Register here
+        </a>
       </div>
     </section>
   );
