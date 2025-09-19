1c62ab9fd03b3af7f54f5438e0d57a05914e71fb
MI KE <fernandesmandrew@gmail.com>
2025-09-19 09:45:07 +0800

    Initial testing. API V1 passed

---
+++ server/routes/noteRoutes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+
+// Get all notes handler
+router.get("/", (req, res) => {
+  res.json({
+    msg: "Welcome back!",
+    version: "V1",
+    year: "2025",
+    route: "/",
+  });
+});
+
+// Get a single note handler
+router.get("/:id", (req, res) => {
+  res.json({
+    msg: "You requesting for a specific note!",
+    version: "V1",
+    year: "2025",
+    route: "/:id",
+  });
+});
+
+// Add a new note handler
+router.post("/", (req, res) => {
+  res.json({
+    msg: "You are posting a new note!!",
+    version: "V1",
+    year: "2025",
+    route: "/ but POST method",
+  });
+});
+
+// Replace an existing note handler
+router.put("/:id", (req, res) => {
+  res.json({
+    msg: "You are replacing n existing note!!",
+    version: "V1",
+    year: "2025",
+    route: "/:id",
+  });
+});
+
+// Update an existing note handler
+router.patch("/:id", (req, res) => {
+  res.json({
+    msg: "You are updating an existing note!!",
+    version: "V1",
+    year: "2025",
+    route: "/:id",
+  });
+});
+
+// Delete an existing note handler
+router.delete("/:id", (req, res) => {
+  res.json({
+    msg: "You are deleting an existing note!!",
+    version: "V1",
+    year: "2025",
+    route: "/:id",
+  });
+});
+
+// Export the router, so that it can be mounted on the main server that has the express() instance
+// No need to desctructure because you are exporting only one Object
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+// Module imports
 require("dotenv").config();
 const express = require("express");
-
-
-
+const routes = require("./routes/noteRoutes");
+const CORS = require("cors");
 const app = express();
+
+// Variables
 const PORT = process.env.PORT;
 
 // Middleware for logging each requests
@@ -12,10 +14,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ msg: "test" });
-});
+// Express don't parse REQUESTS Objects' contents that has JSON (application/json) in its header (Content-Type header). It reads the raw JSON payload from the request body and converts it into a usable JavaScript object, which is then stored in req.body
+// Allow Cross-Origin Resource Sharing (CORS) on all routes. Both fronted and backend runs on distinct ports, so resource sharing might be blocked as part of basic browser security.
+app.use(express.json());
+app.use(CORS());
+
+// Mount or consume all the routes inside
+// If a path was specified (as first argument), it appends the all the routes unto that path or a route that matches it. Otherwise, it consumes all the routes inside. e.g. app.use("/user", route), all the routes inside route will append to the "/user" path, which results to "/user/routeInside"
+app.use(process.env.BASE_API_URL, routes);
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
+  console.log(`Base URL: ${process.env.BASE_API_URL}`);
 });
