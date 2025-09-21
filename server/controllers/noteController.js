47d0dacc766b01b59f4cff24f4128bb7a1328636
MI KE <fernandesmandrew@gmail.com>
2025-09-22 04:47:26 +0800

    Updated correct collection for database and updated addNote controller

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -20,14 +20,27 @@ const getSingleNote = (req, res) => {
   });
 };
 
-const addNote = (req, res) => {
-  res.json({
-    msg: "Adding a single note!",
-    version: "V1",
-    year: "2025",
-    route: "/",
-    handler: "addNote",
-  });
+const addNote = async (req, res) => {
+  try {
+    // Deconstruct contents from request body to avoid using "this" and unneeded variable declaration
+    const { title, from, contents } = req.body;
+
+    // Creating Note object allows more control, as we can insert hooks and manually call save()
+    const newNote = new Note({ title, from, contents });
+
+    // Returns a Promise and triggers the pre() hook on the Note schema
+    const savedNote = await newNote.save();
+
+    // Handle the Promise
+    res.status(201).json({
+      message: `Note: ${savedNote.title}, was created and saved succesfully!`,
+      note: savedNote,
+    });
+  } catch (error) {
+    // Error handlers maybe passed
+    res.status(400).json({ error: error.message });
+    console.log(error);
+  }
 };
 
 const updateNote = (req, res) => {
--- server/models/Note.js
+++ server/models/Note.js
@@ -9,4 +9,13 @@ const noteSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Mongoose hooks that executes the function during document pre-save
+// No to arrow functions, their lexical scope messes with the "this" keyword
+// Triggered by an async handler, so make sure its async
+noteSchema.pre("save", async function () {
+  // Import and create from /utils folder if logic gets complex here
+  console.log(`Saving: ${this.title}`);
+  // No need to use next() since the function is async
+});
+
 module.exports = mongoose.model("Note", noteSchema);
