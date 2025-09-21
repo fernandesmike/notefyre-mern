452eb4333c75b1d0d5a54c480758c396d663dcba
MI KE <fernandesmandrew@gmail.com>
2025-09-22 06:00:33 +0800

    Updated getSingleNote handler

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -1,25 +1,51 @@
+require("mongoose");
+const { default: mongoose } = require("mongoose");
 const Note = require("../models/Note");
 
 const getAllNotes = async (req, res) => {
   try {
     //  Sorting, -1 for Descending and 1 for Ascending
-    // Returns a Query Object (from mongoose)
-    const allNotes = await Note.find().sort({ createdAt: -1 });
-    res.status(200).json({ message: "All notes retrieved!", allNotes });
+    //  Using lean() will only return plain JS Object not a Mongoose Document
+    const allNotes = await Note.find().sort({ createdAt: -1 }).lean();
+    res.status(200).json({
+      message: "All notes retrieved!",
+      count: allNotes.length,
+      allNotes,
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
     console.log(error);
   }
 };
 
-const getSingleNote = (req, res) => {
-  res.json({
-    msg: "Retrieving a single note!",
-    version: "V1",
-    year: "2025",
-    route: "/:id",
-    handler: "getSingleNote",
-  });
+const getSingleNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    //  Check first if the provided ID is a valid MongoDB ID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(404)
+        .json({ message: "Invalid ID. Please check the provided ID" });
+    }
+
+    const singleNote = await Note.findById(id).lean();
+
+    // If there is no such note, then return 404
+    if (!singleNote) {
+      return res
+        .status(404)
+        .json({ message: "Cannot find the note with the specified ID" });
+    }
+
+    res.status(200).json({
+      message: `Note: ${singleNote.title} retrieved.`,
+      singleNote,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.log(error);
+  }
 };
 
 const addNote = async (req, res) => {
