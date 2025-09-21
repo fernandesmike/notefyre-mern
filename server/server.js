2f1d75643f545665f277d918919b605cc40ebb94
MI KE <fernandesmandrew@gmail.com>
2025-09-22 03:09:27 +0800

    Added logger middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const routes = require("./routes/noteRoutes");
 const connectDb = require("./config/mongoDb");
+const logger = require("./middleware/logger");
 const express = require("express");
 const CORS = require("cors");
 const PORT = process.env.PORT;
@@ -13,10 +14,7 @@ app.use(express.json());
 app.use(CORS());
 
 // Middleware for logging each requests
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+app.use(logger);
 
 // Mount or consume all the routes inside
 // If a path was specified (as first argument), it appends the all the routes unto that path or a route that matches it. Otherwise, it consumes all the routes inside. e.g. app.use("/user", route), all the routes inside route will append to the "/user" path, which results to "/user/routeInside"
